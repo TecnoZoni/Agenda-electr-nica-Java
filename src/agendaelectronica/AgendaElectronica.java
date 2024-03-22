23f342ea4674298e87902047766e992192eb4217
Agustin Minzoni <90013026+TecnoZoni@users.noreply.github.com>
2024-03-22 07:22:47 -0300

    Add files via upload

---
+++ src/agendaelectronica/AgendaElectronica.java
@@ -0,0 +1,13 @@
+package agendaelectronica;
+
+import agendaelectronica.igu.Principal;
+
+public class AgendaElectronica {
+
+    public static void main(String[] args) {
+        Principal panel = new Principal();
+        panel.setVisible(true);
+        panel.setLocationRelativeTo(null);
+    }
+    
+}
